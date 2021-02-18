58fbcc123b80f41dcbf1874b529ed0c9d4f7ea36
Agus Riady <riadyagus1@gmail.com>
2021-02-18 17:39:53 +0700

    Initial Commit

---
+++ SAMPolibatam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SAMPolibatam
+//
+//  Created by Agus Riady on 18/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
