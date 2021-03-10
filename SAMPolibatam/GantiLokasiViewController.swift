58dae0e1a1153def3aabcdcbf697fcac7ff64cf8
Agus Riady <riadyagus1@gmail.com>
2021-03-10 16:35:54 +0700

    Perubahan Menu: Ganti Password -> Ganti Lokasi Rumah

---
+++ SAMPolibatam/GantiLokasiViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GantiLokasiViewController.swift
+//  SAMPolibatam
+//
+//  Created by Agus Riady on 10/03/21.
+//
+
+import UIKit
+import MapKit
+
+class GantiLokasiViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtn(_ sender: UIButton) {
+        if let navController = self.navigationController {
+          navController.popViewController(animated: true)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
