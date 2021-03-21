5823910be10952e0910c5557f2774107dfdb2dc1
Agus Riady <riadyagus1@gmail.com>
2021-03-21 21:51:23 +0700

    Dragable Marker + Get Coordinate From Marker

--- SAMPolibatam/GantiLokasiViewController.swift
+++ SAMPolibatam/GantiLokasiViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class GantiLokasiViewController: UIViewController, GMSAutocompleteResultsViewControllerDelegate {
+class GantiLokasiViewController: UIViewController, GMSAutocompleteResultsViewControllerDelegate, GMSMapViewDelegate {
 
     @IBOutlet var mapView: GMSMapView!
     
@@ -34,7 +34,24 @@ class GantiLokasiViewController: UIViewController, GMSAutocompleteResultsViewCon
         setupSearchController()
         resultsViewController?.delegate = self
         // Search (End)
+        
+        let marker = GMSMarker()
+        marker.isDraggable = true
+        marker.map = mapView
+        self.mapView.delegate = self
     }
+    
+    //MARK: Simpan Coordinate
+    var latitudeNow:Double?
+    var longitudeNow:Double?
+    
+    //MARK: Get Coordinate From Marker
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        print("Position of marker is = \(marker.position.latitude),\(marker.position.longitude)")
+        self.latitudeNow = marker.position.latitude
+        self.longitudeNow = marker.position.longitude
+    }
+    // Get Coordinate From Marker (End)
     
     //MARK: Search
     func setupSearchController() {
@@ -57,13 +74,16 @@ class GantiLokasiViewController: UIViewController, GMSAutocompleteResultsViewCon
         mapView.animate(toLocation: place.coordinate)
         let marker = GMSMarker(position: place.coordinate)
         marker.title = place.name
+        marker.isDraggable = true
         marker.map = mapView
         mapView.camera = GMSCameraPosition.camera(withTarget: place.coordinate, zoom: 17)
+        self.latitudeNow = place.coordinate.latitude
+        self.longitudeNow = place.coordinate.longitude
     }
-       
-       func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didFailAutocompleteWithError error: Error) {
-           print("Error: \(error.localizedDescription)")
-       }
+    
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+    }
     // Search (End)
     
     private let locationManager = CLLocationManager()
@@ -79,24 +99,50 @@ class GantiLokasiViewController: UIViewController, GMSAutocompleteResultsViewCon
     
     //MARK: Confirm Button
     @IBAction func gantiLokasi(_ sender: UIButton) {
-        let dialogMessage = UIAlertController(title: "Ganti Lokasi Rumah Berhasil", message: "\n Pergantian lokasi rumah berhasil dan lokasi rumah yang baru telah disimpan.", preferredStyle: .alert)
-        
-        // Create OK button with action handler
-        let ok = UIAlertAction(title: "Selesai", style: .default, handler: { (action) -> Void in
-            print("Ganti Lokasi Rumah Success")
-            if let navController = self.navigationController {
-              navController.popViewController(animated: true)
-            }
-            self.dismiss(animated: true, completion: nil)
-         })
-        
-        //Add OK button to a dialog message
-        dialogMessage.addAction(ok)
-        
-        // Present Alert to
-        self.present(dialogMessage, animated: true, completion: nil)
+        if longitudeNow == nil {
+            let dialogMessage = UIAlertController(title: "Ganti Lokasi Rumah Gagal", message: "\n Pilih Lokasi terlebih dahulu. Silahkan coba lagi!", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "Kembali", style: .default, handler: { (action) -> Void in
+                print("Kembali button tapped")
+             })
+            
+            //Add OK button to a dialog message
+            dialogMessage.addAction(ok)
+            // Present Alert to
+            self.present(dialogMessage, animated: true, completion: nil)
+        } else {
+            let dialogMessage = UIAlertController(title: "Ganti Lokasi Rumah Berhasil", message: "\n Pergantian lokasi rumah berhasil dan lokasi rumah yang baru telah disimpan.\n\n Latitude = \(latitudeNow ?? 0.0) \n Longitude = \(longitudeNow ?? 0.0)", preferredStyle: .alert)
+            
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "Selesai", style: .default, handler: { (action) -> Void in
+                print("Ganti Lokasi Rumah Success")
+                if let navController = self.navigationController {
+                  navController.popViewController(animated: true)
+                }
+                self.dismiss(animated: true, completion: nil)
+             })
+            
+            //Add OK button to a dialog message
+            dialogMessage.addAction(ok)
+            
+            // Present Alert to
+            self.present(dialogMessage, animated: true, completion: nil)
+        }
     }
     // Confirm Button (End)
+    
+    //MARK: Information Button
+    @IBAction func infoButton(_ sender: UIButton) {
+        let dialogMessage = UIAlertController(title: "Tutorial Menganti Lokasi Rumah", message: "\n1. Search Lokasi Rumah. \n 2. Tahan pin lokasi lalu pindahkan sesuai ke titik rumah anda. \n 3. Setelah pin sesuai dengan lokasi rumah anda, Tekan tombol 'Ganti Lokasi Rumah' untuk simpan." , preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "Mengerti", style: .default, handler: { (action) -> Void in
+            print("Mengerti button tapped")
+         })
+        
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    // Information Button (End)
     
 }
 
@@ -125,4 +171,3 @@ extension GantiLokasiViewController: CLLocationManagerDelegate {
   }
 }
 // Zoom To Current Location (End)
-
